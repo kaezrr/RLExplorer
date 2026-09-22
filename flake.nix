ac0e5e44bd9d7a65cecf2a33502f87c527c49f14
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-09-22 11:01:13 +0530

    add direnv and flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
+    llm-agents.url = "github:numtide/llm-agents.nix";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      llm-agents = inputs.llm-agents.packages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.godot
+          llm-agents.freebuff
+        ];
+      };
+    };
+}
